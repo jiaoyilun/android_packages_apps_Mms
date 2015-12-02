c6ce46e1649e36c58f7bbe7fc66031dec99755f7
jiaoyilun <jiaoyilun@gmail.com>
2015-12-02 14:29:41 +0800

    add gesture support
    Change-Id: If4b5ffa6962de5ee9f1324aa1f170233cc6c7071

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -4238,10 +4238,10 @@ public class ComposeMessageActivity extends Activity
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            if (s.toString().getBytes().length <= SUBJECT_MAX_LENGTH) {
+            if (s.toString().length() <= SUBJECT_MAX_LENGTH) {
                 mWorkingMessage.setSubject(s, true);
                 updateSendButtonState();
-                if (s.toString().getBytes().length == SUBJECT_MAX_LENGTH
+                if (s.toString().length() == SUBJECT_MAX_LENGTH
                         && before < SUBJECT_MAX_LENGTH) {
                     String toast = getString(R.string.subject_full, SUBJECT_MAX_LENGTH);
                     Toast.makeText(ComposeMessageActivity.this, toast,
@@ -4252,13 +4252,11 @@ public class ComposeMessageActivity extends Activity
 
         @Override
         public void afterTextChanged(Editable s) {
-            if (s.toString().getBytes().length > SUBJECT_MAX_LENGTH) {
-                String subject = s.toString();
+            if (s.toString().length() > SUBJECT_MAX_LENGTH) {
                 String toast = getString(R.string.subject_full, SUBJECT_MAX_LENGTH);
                 Toast.makeText(ComposeMessageActivity.this, toast,
                         Toast.LENGTH_SHORT).show();
-                s.clear();
-                s.append(subject.substring(0, SUBJECT_MAX_LENGTH));
+                s.delete(SUBJECT_MAX_LENGTH - 1, s.length());
             }
         }
     };
@@ -5710,6 +5708,19 @@ public class ComposeMessageActivity extends Activity
                     item.setVisible(false);
                 }
             }
+
+            int checkedCount = getListView().getCheckedItemCount();
+            if (checkedCount > 0) { // action mode was restarted
+                updateCheckedStatus(mode, checkedCount);
+                // loop through checked items and update statistics
+                SparseBooleanArray checkedList = getListView().getCheckedItemPositions();
+                for (int i = 0; i < checkedList.size(); i++) {
+                    int position = checkedList.keyAt(i);
+                    boolean isChecked = checkedList.valueAt(i);
+                    updateStatics(position, isChecked);
+                    customMenuVisibility(mode, checkedCount, position, isChecked);
+                }
+            }
             return true;
         }
 
@@ -6124,7 +6135,7 @@ public class ComposeMessageActivity extends Activity
 
         @Override
         public void onItemCheckedStateChanged(ActionMode mode, int position,
-                long id, boolean checked) {
+                                              long id, boolean checked) {
             logMultiChoice("onItemCheckedStateChanged... position=" + position
                     + ", checked=" + checked);
 
@@ -6134,6 +6145,13 @@ public class ComposeMessageActivity extends Activity
             mode.setTitle(getString(R.string.selected_count, mCheckedCount));
 
             mode.getMenu().findItem(R.id.selection_toggle).setTitle(getString(
+                    allItemsSelected() ? R.string.deselected_all : R.string.selected_all));
+        }
+
+        private void updateCheckedStatus(ActionMode actionMode, int checkedCount) {
+            mCheckedCount = checkedCount;
+            actionMode.setTitle(getString(R.string.selected_count, mCheckedCount));
+            actionMode.getMenu().findItem(R.id.selection_toggle).setTitle(getString(
                     allItemsSelected() ? R.string.deselected_all : R.string.selected_all));
         }
 
@@ -6195,6 +6213,7 @@ public class ComposeMessageActivity extends Activity
         t.run();
     }
 
+    @Override
     public void onPickup() {
         if (!getRecipients().isEmpty()) {
             mProximitySensorManager.disable();
